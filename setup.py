8dda801891bfc5e462f8202bf419b0d19fd93395
Stefan Taubert <stefan.taubert@posteo.de>
2020-12-03 10:58:08 +0100

    - updated to version 1.0.1 - updated setup.py - installed pipenv-setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='cmudict_parser',
-    version='1.0.0',
-    url='https://github.com/stefantaubert/cmudict-parser.git',
-    author='Stefan Taubert',
-    author_email='stefan.taubert@posteo.de',
-    description='Python parser for CMUDict files. It returns ARBAbet and IPA transciption of dictionary words.',
+    name="cmudict_parser",
+    version="1.0.1",
+    url="https://github.com/stefantaubert/cmudict-parser.git",
+    author="Stefan Taubert",
+    author_email="stefan.taubert@posteo.de",
+    description="Python parser for CMUDict files. It returns ARBAbet and IPA transciption of dictionary words.",
     packages=["cmudict_parser"],
-    install_requires=["tqdm", "wget"],
+    install_requires=["tqdm==4.54.0", "wget==3.2"],
 )
