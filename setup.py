d051e268876278391c020843c941d137aaed5f9b
Stefan Taubert <stefan.taubert@posteo.de>
2021-06-01 09:28:25 +0200

    update

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name="cmudict_parser",
-    version="1.0.1",
-    url="https://github.com/stefantaubert/cmudict-parser.git",
-    author="Stefan Taubert",
-    author_email="stefan.taubert@posteo.de",
-    description="Python parser for CMUDict files. It returns ARBAbet and IPA transciption of dictionary words.",
-    packages=["cmudict_parser"],
-    install_requires=["tqdm>=4.54.0", "wget>=3.2"],
-)
+if __name__ == "__main__":
+  setuptools.setup()
